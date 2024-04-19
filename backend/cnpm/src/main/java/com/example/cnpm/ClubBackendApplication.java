f84cbcd39b752edf3c196dc83e3b44de867ec075
Thành Duy <leeduck29@gmail.com>
2024-04-20 01:59:08 +0700

    feat: add some unadded file

---
+++ backend/cnpm/src/main/java/com/example/cnpm/ClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/cnpm/src/test/java/com/example/cnpm/ClubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
